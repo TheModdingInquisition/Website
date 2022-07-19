38200bf170602294703bd2ec77a73599152fe6c7
Matyrobbrt <matyrobbrt@gmail.com>
2022-07-19 20:28:27 +0300

    Initial project setup

---
+++ backend/src/main/groovy/org/moddinginquisition/web/backend/Configuration.groovy
@@ -0,0 +1,58 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022 Modding Inquisition
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package org.moddinginquisition.web.backend
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import groovy.transform.CompileStatic
+import groovy.transform.ToString
+import groovy.util.logging.Slf4j
+
+import java.nio.file.Files
+import java.nio.file.Path
+
+@Slf4j
+@ToString
+@CompileStatic
+class Configuration {
+    private static final Gson GSON = new GsonBuilder()
+        .setPrettyPrinting()
+        .setLenient()
+        .disableHtmlEscaping()
+        .create()
+
+    int port = 8080
+
+    static Configuration read(Path path) throws IOException {
+        if (Files.notExists path) {
+            try (final writer = path.newWriter()) {
+                GSON.toJson(new Configuration(), writer)
+            }
+        }
+        try (final is = path.newReader()) {
+            GSON.fromJson(is, Configuration)
+        }
+    }
+}
